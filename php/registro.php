c557d984c1afc516f12f976945ab4255a6f9b94d
ReidakaiMFZ <richard_maldito2@hotmail.com>
2021-08-18 17:28:24 -0300

    arrumando a conexão

--- php/registro.php
+++ php/registro.php
@@ -1,13 +1,13 @@
 <?php
-if($_GET['nome'] !="" && $_GET['Senha'] !="" && $_GET['email'] !="" && $_GET['sobrenome'] !="")
+if($_GET['nome'] !="" && $_GET['Senha'] !="" && $_GET['email'] !="" && $_GET['login'] !="")
 {
     $oConexao = mysqli_connect('localhost', 'Aluno2DS', 'SenhaBD2', 'BANCOCOMUM');
-    $oConsulta = "INSERT INTO USUARIOS(USRNOME, USRSENHA, USREMAIL, USRSOBRENOME) VALUES ('".$_GET['nome']."', MD5('".$_GET['Senha']."'),'" .$_GET['email']."','". $_GET['sobrenome'] . "' )";
+    $oConsulta = "INSERT INTO USUARIOS(USRNOME, USRSENHA, USREMAIL, USRLOGIN) VALUES ('".$_GET['nome']. "', MD5('".$_GET['Senha']."'),'" .$_GET['email']."','". $_GET['login'] . "')";
 
     $oQuery = mysqli_query($oConexao, $oConsulta);
         
     mysqli_Close($oConexao);
-    header('location: ../index.html');
+    header('location: ../login.htm');
 }
 else 
 {
