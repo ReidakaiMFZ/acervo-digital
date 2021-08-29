774fd8be3bf624f01583e2f4293ee2d6f3c15c71
ReidakaiMFZ <richard_maldito2@hotmail.com>
2021-08-29 16:36:57 -0300

    adição do main

--- php/index.php
+++ php/index.php
@@ -2,10 +2,12 @@
 session_start();
 
 $conexao = mysqli_connect("localhost", "root", "","acervo");
-$query = "SELECT MSCNOME, MSCDURACAO, MSCBANDA, MSCARTISTA FROM musicas";
-$consultas = mysqli_query($conexao, $query);
-$total = mysqli_num_rows($consultas);
-$contador = 0;
+$queryMusica = "SELECT MSCNOME, MSCDURACAO, BDSNOME, ARTNOME
+FROM musicas 
+LEFT JOIN BANDAS ON BANDAS.BDSCODIGO = MUSICAS.MSCBANDA 
+LEFT JOIN ARTISTAS ON ARTISTAS.ARTCODIGO = MUSICAS.MSCARTISTA ";
+$consultaMusicas = mysqli_query($conexao, $queryMusica);
+
 ?>
 
 <!DOCTYPE html>
@@ -15,52 +17,60 @@ $contador = 0;
   <meta name="viewport" content="width=device-width">
   <link rel="stylesheet" href="../css/cssinicio.css">
   <script src="../js/indexscript.js"></script>
-  <title>inicial</title>
+  <title>Inicio - ACERVO</title>
 </head>
 
 <body>
 
-  <header>
-    <h3><a href="#">Inicio</a></h3>
-    <h3><a href="#">Biblioteca</a></h3>
-    <h3><a href="#">Cadastrar</a></h3>
+<header>
 
-    <div class="pesquisa">
-      <input id="txtPesquisa" type="search" class="txtPesquisa" placeholder="Pesquisar..." />
-      <input type="image" class="btnPesquisa" src="../images/search-site-pw2.png" alt="">
-    </div>
+  <h3><a href="#">Inicio</a></h3>
+  <h3><a href="#">Biblioteca</a></h3>
+  <h3><a href="#">Cadastrar</a></h3>
 
-    <div class="perfil">
-      <img src="../images/unknown.ico">
-      <h3>
-        <a href="#">perfil</a>
-      </h3>
-    </div>
-    
-  </header>
+  <div class="pesquisa">
+    <input id="txtPesquisa" type="search" class="txtPesquisa" placeholder="Pesquisar..." />
+    <input type="image" class="btnPesquisa" src="../images/search-site-pw2.png" alt="">
+  </div>
 
-  <main>
-      <?php
-          while($reg = mysqli_fetch_assoc($consultas)){
-            echo "<div id='fileira" . $contador . "'>";
-            echo "<img src='alguma imagem aí'>";
-            echo "<label>" . $reg['MSCNOME'] . "</label>";
-            if($reg['MSCARTISTA'] != null){
-              echo "<small>" . $reg['MSCARTISTA'] . "</small>";
-            }
-            else{
-              echo "<small>" . $reg['MSCBANDA'] . "</small>";
-            }
-            echo "</div>";
-            $contador++;
+  <div class="perfil">
+    <img src="../images/unknown.ico">
+    <h3>
+      <a href="#">perfil</a>
+    </h3>
+  </div>    
+</header>
+
+<div class="linha">
+  <h2>Músicas</h2>
+  <table>
+    <tbody>
+      <tr>
+        <?php
+        for($i = 0; $i<=6; $i++){
+          $regMusicas = mysqli_fetch_assoc($consultaMusicas);
+          echo "<td>";
+          echo "<div class='album'>";
+          echo   "<a href=''><img src='../images/placeholder-de-imagens.png'/>";
+          echo   "<label>" . $reg['MSCNOME'] . "</label></a>";
+          echo "<br/>";
+          if ($reg['BDSNOME'] == NULL){
+            echo   "<a href=''><small>" . $regMusicas['ARTNOME'] . "</small></a>";
           }
-          mysqli_free_result($consultas);
-          mysqli_close($conexao);
+          else{
+            echo   "<a href=''><small>" . $regMusicas['BDSNOME'] . "</small></a>";
+          }
+          echo "</td>";
+          echo "</div>";
+        }
+        mysqli_free_result($consultaMusicas);
+        mysqli_close($conexao);
         ?>
-      
-  </main>
-
-
+      </tr>
+    </tbody>
+  </table>
+</div>
 
 </body>
+
 </html>
